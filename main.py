e9040642ab2a80b6ce540783501be9a12daf59f1
Rishil Kadakia <rishil.kadakia@gmail.com>
2021-06-02 19:42:10 -0700

    Added dimensions for GUI.

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from tkinter import ttk
 from tkinter import filedialog
 from pytube import YouTube
 
-root = Tk()
-root.title('YouTube Video Downloader - By Rishil')
+root = Tk()                                                                                                             # Creates window.
+root.title('YouTube Video Downloader - By Rishil')                                                                      # Sets window title.
+root.geometry('400x400')                                                                                                # Sets dimensions of window to 400 pixels by 400 pixels.
+root.columnconfigure(0, weight = 1)                                                                                     # Centers everything.
 
 root.mainloop()
