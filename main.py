70b668c96729528e803764c45df8046f93412312
Rishil Kadakia <rishil.kadakia@gmail.com>
2021-06-05 14:40:47 -0700

    Added video url label and url entry box to GUI.

--- main.py
+++ main.py
@@ -8,4 +8,11 @@ root.title('YouTube Video Downloader - By Rishil')                              
 root.geometry('400x400')                                                                                                # Sets dimensions of window to 400 pixels by 400 pixels.
 root.columnconfigure(0, weight = 1)                                                                                     # Centers everything.
 
+url_label = Label(root, text='Video URL:', font=('Arial', 14))                                                          # Video URL label.
+url_label.grid()                                                                                                        # Adding video url label to the GUI.
+
+url = StringVar()                                                                                                       # Setting video url to string.
+url_box = Entry(root, width = 50, textvariable = url)                                                                   # Creating entry box.
+url_box.grid()                                                                                                          # Adding entry box to the GUI.
+
 root.mainloop()
