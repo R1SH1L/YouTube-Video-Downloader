33663e073a71a1474f80d3dcf76bfd897d02c5e7
Rishil Kadakia <rishil.kadakia@gmail.com>
2021-06-07 01:39:55 -0700

    Added error message for video url, save video label, choose path button, and error message for path choosing.

--- main.py
+++ main.py
@@ -8,11 +8,35 @@ root.title('YouTube Video Downloader - By Rishil')                              
 root.geometry('400x400')                                                                                                # Sets dimensions of window to 400 pixels by 400 pixels.
 root.columnconfigure(0, weight = 1)                                                                                     # Centers everything.
 
-url_label = Label(root, text='Video URL:', font=('Arial', 14))                                                          # Video URL label.
+# "Video URL" Label
+url_label = Label(root, text='Video URL:', fg = 'Blue', font=('Arial', 14))                                             # Video URL label.
 url_label.grid()                                                                                                        # Adding video url label to the GUI.
 
+# Video url that gets inputted.
 url = StringVar()                                                                                                       # Setting video url to string.
 url_box = Entry(root, width = 50, textvariable = url)                                                                   # Creating entry box.
 url_box.grid()                                                                                                          # Adding entry box to the GUI.
+
+# Error Message for Video URL
+url_error = Label(root, text = 'Error', fg = 'Red', font = ('Arial', 10))
+url_error.grid()
+
+# "Save Video" Label
+save_label = Label(root, text='Save Location:', fg = 'Blue', font = ('Arial', 14))                                      # Save Video label.
+save_label.grid()                                                                                                       # Adding save video label to the GUI.
+
+# Choose Path Button
+path_button = Button(root, width = 10, bg = 'Red', fg = 'White', text = 'Choose Path')
+path_button.grid()
+
+# Error Message for Path Choosing
+path_error = Label(root, text = 'Error of Path', fg = 'Red', font = ('Arial', 10))
+path_error.grid()
+
+
+download_quality = Label(root, text = 'Select Video Quality:', fg = 'Blue', font = ('Arial', 14))
+download_quality.grid()
+
+
 
 root.mainloop()
